ef5f5295c28907e0448324ed49bf393bd67e8c1c
MUKESH KUMAR PATEL <kumarmukeshpatel57@gmail.com>
2021-05-16 12:22:48 +0530

    use of library

--- app/src/main/java/com/mukeshkpdeveloper/easytoasterexample/MainActivity.java
+++ app/src/main/java/com/mukeshkpdeveloper/easytoasterexample/MainActivity.java
@@ -3,6 +3,10 @@ package com.mukeshkpdeveloper.easytoasterexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mukeshkpdeveloper.easytoasterlibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnShowMsg = findViewById(R.id.msgShowBtn);
+
+        btnShowMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToasterMessage.s(MainActivity.this, "Thanks for Click here");
+            }
+        });
+
+
+
     }
 }
